30cfe70e91b2151a07e789df4c5b3ef31f7c47db
elmasryn <suspecious002@hotmail.com>
2022-01-22 19:03:40 +0200

    seeding active

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,21 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
-        // $this->call(RoleSeeder::class);
-        // $this->call(RoleUserSeeder::class);
-        // $this->call(SettingSeeder::class);
-        // $this->call(Message_titleSeeder::class);
-        // $this->call(MessageSeeder::class);
-        // $this->call(ProfileSeeder::class);
-        // $this->call(CategorySeeder::class);
-        // $this->call(PageSeeder::class);
-        // $this->call(PostSeeder::class);
-        // $this->call(TagSeeder::class);
-        // $this->call(PostTagSeeder::class);
-        // $this->call(CommentSeeder::class);
-        // $this->call(Website_content_areaSeeder::class);
-        // $this->call(Website_contentSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(RoleUserSeeder::class);
+        $this->call(SettingSeeder::class);
+        $this->call(Message_titleSeeder::class);
+        $this->call(MessageSeeder::class);
+        $this->call(ProfileSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(PageSeeder::class);
+        $this->call(PostSeeder::class);
+        $this->call(TagSeeder::class);
+        $this->call(PostTagSeeder::class);
+        $this->call(CommentSeeder::class);
+        $this->call(Website_content_areaSeeder::class);
+        $this->call(Website_contentSeeder::class);
 
     }
 }
